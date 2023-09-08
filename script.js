593ce4327d8418829b5e1f514ca3ef5ecd8ff5a2
KEINance <keinance2@gmail.com>
2023-09-08 09:47:52 -0400

    timer, right wrong

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const scoringContainer = document.getElementById('scoring-container');
 const button = document.getElementById('button');
 const highscoresContainer = document.getElementById('highscores-container');
 const playerScores = json.parse(localStorage.getItem('playerScores'));
-
+const nxtBtn = document.getElementById('nxtBtn');
 // code questions and answers
 const questionList [
     {
@@ -53,18 +53,58 @@ const questionList [
 ];
 // localstorage 
 localStorage.setItem('playerScores', json.stringify(playerScores));
+//start quiz
+
 // countdown
+function countDown() {
+  
+  timeInterval = setInterval( function (){
+    timeRemaining--;
+    timer.textContent = timeRemaining + 'remaining until quiz ends!';
 
+    if (timeRemaining === 0) {
+        clearInterval(timeInterval)
+        playerScores();
+    }
+}, 1000)
+}
 // penalty counter
-
+function penalty(time) {
+    secRemaining -= time;
+    if(secRemaining < 0) {
+        secRemaining = 0;
+    }
+}
 //show questions
-
+function questions() {
+    if ( questionList = questions.length) {
+        clearInterval(timeInterval)
+        playerScores();
+        return;
+    }
+}
 //show right answer 'correct' or 'wrong' statement
-
+function rightWrong() {
+    const answerChoice = e.target;
+    const right = answerChoice.dataset.correct;
+    answerChoice.classList.remove('hide');
+    if (right) {
+        answerChoice.innerHTML = 'CORRECT!';
+    } else {
+    // wrong answers decreae time by 5 seconds
+        answerChoice.innerHTML = 'INCORRECT~';
+        if (timeRemaining <= 5) {
+            timeRemaining = 0;
+        } else {
+            timeRemaining -= 5;
+        }
+    }
+}
 // show highscores
 
 //clear screen
 
-//start button
+//start button, highscores, add score
 startBtn.addEventListener('click', startQuiz);
+playerScores.addEventListener('click', saveScore)
 highscoreBtn.addEventListener('click', highscore);
